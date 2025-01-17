7277f7b4a38d8491113410491104e3c7541ab1ac
Almaz Mingaleev <mingaleev@google.com>
2025-01-17 12:25:33 +0000

    DO NOT MERGE Update Android ICU data from 2024a to 2024b. [T]
    Cherry-pick: ICU-22896 updating tzdata to 2024b
    
    Upstream commit:
    https://github.com/unicode-org/icu/commit/1c2a1c50b70be5bf34ece49b43165653c4aecb0a
    
    Partial cherry-pick: Time zone tests from CLDR 46
    
    Upstream commit:
    https://github.com/unicode-org/icu/commit/d04154833c44b55257f365072234a58eb599ef77
    
    This commit updates icudt72l.dat file. It's caused by fr_CA and
    nn.txt updates: with those changes reverted .dat file is left
    intact.
    
    U and aosp/main changes update sw_KE, but T's version of it does
    not have Choibalsan meta zone entry hence it is not updated here.
    
    Bug: 390353440
    
    Test: run cts --include-filter CtsLibcoreTestCases
    Test: run cts --include-filter CtsLibcoreOjTestCases
    Test: run cts --include-filter CtsIcuTestCases
    Test: run cts --include-filter CtsBionicTestCases
    Test: run cts --include-filter CtsTextTestCases
    Test: atest MtsTimeZoneDataTestCases
    
    Change-Id: I410c67058acf5f08272378dd23e0b0a10d8bedf8
    Merged-In: I0b36f30f0e5695801d7025fc6dece10cac8800a2
    Merged-In: I25a2024c40b0bde28ef581f9f732f8860f774dfd
    Merged-In: I293690eed77261ccf6d38c4771cf0f1a48f6642a

--- icu4c/source/test/intltest/tztest.cpp
+++ icu4c/source/test/intltest/tztest.cpp
@@ -1301,7 +1301,6 @@ TimeZoneTest::TestAliasedNames()
         {"Chile/Continental", "America/Santiago"},
         {"Chile/EasterIsland", "Pacific/Easter"},
         {"Cuba", "America/Havana"},
-        {"EST", "Etc/GMT+5"},
         {"Egypt", "Africa/Cairo"},
         {"Eire", "Europe/Dublin"},
         {"Etc/GMT+0", "Etc/GMT"},
@@ -1321,7 +1320,6 @@ TimeZoneTest::TestAliasedNames()
         {"GMT-0", "Etc/GMT"},
         {"GMT0", "Etc/GMT"},
         {"Greenwich", "Etc/GMT"},
-        {"HST", "Etc/GMT+10"},
         {"Hongkong", "Asia/Hong_Kong"},
         {"Iceland", "Atlantic/Reykjavik"},
         {"Iran", "Asia/Tehran"},
@@ -1330,7 +1328,6 @@ TimeZoneTest::TestAliasedNames()
         {"Japan", "Asia/Tokyo"},
         {"Kwajalein", "Pacific/Kwajalein"},
         {"Libya", "Africa/Tripoli"},
-        {"MST", "Etc/GMT+7"},
         {"Mexico/BajaNorte", "America/Tijuana"},
         {"Mexico/BajaSur", "America/Mazatlan"},
         {"Mexico/General", "America/Mexico_City"},
@@ -1413,7 +1410,7 @@ TimeZoneTest::TestAliasedNames()
             from->getDisplayName(fromName);
             to->getDisplayName(toName);
             if(fromName.compare(toName) != 0) {
-                errln("Fail: Expected "+toName+" but got " + fromName);
+                errln("Fail: Expected " + toName + " but got " + fromName + " for index: "+ i);
             }
         }
         delete from;
@@ -2422,7 +2419,7 @@ void TimeZoneTest::TestGetRegion()
         {"America/Indiana/Indianapolis",    "US"},  // CLDR alias
         {"Mexico/General",                  "MX"},  // Link America/Mexico_City, Olson backward
         {"Etc/UTC",                         "001"},
-        {"EST5EDT",                         "001"},
+        {"EST5EDT",                         "US"},
         {"PST",                             "US"},  // Link America/Los_Angeles
         {"Europe/Helsinki",                 "FI"},
         {"Europe/Mariehamn",                "AX"},  // Link Europe/Helsinki, but in zone.tab
